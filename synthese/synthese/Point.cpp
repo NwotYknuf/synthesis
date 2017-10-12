5b1b49cc6bea6feb2963682896bc81286124b581
NwotYknuf <jlafille@ensc.fr>
2017-10-12 16:25:01 +0200

    Point

--- synthese/synthese/Point.cpp
+++ synthese/synthese/Point.cpp
@@ -1,1 +1,17 @@
-//je suis un point
+#pragma once
+#include"point.h"
+
+
+Point::Point(){
+	_x = _y = 0;
+}
+
+Point::Point(int x, int y){
+	_x = x;
+	_y = y;
+}
+
+Point::Point(const Point &p){
+	_x = p._x;
+	_y = p._y;
+}
